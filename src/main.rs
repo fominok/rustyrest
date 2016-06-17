1be188e5c0fb689d063c9f424ef65c69c39878ff
fominok <fominok@hotmail.com>
2016-06-17 09:21:43 +0000

    using ini config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate postgres;
+extern crate ini;
 
-use postgres::{Connection, SslMode};
+use ini::Ini;
+use postgres::{Connection, ConnectParams, ConnectTarget, SslMode, UserInfo};
+
+use std::str::FromStr;
 
 struct Person {
     id: i32,
@@ -8,14 +12,44 @@ struct Person {
     data: Option<Vec<u8>>
 }
 
+fn params<'a>() -> (ConnectParams, SslMode<'a>) {
+    let conf = Ini::load_from_file("phonebookrc").unwrap();
+    let general = conf.general_section();
+
+    let host = general.get("host").unwrap();
+    let port = general.get("port").unwrap();
+    let sslmode = general.get("sslmode").unwrap();
+    let dbname = general.get("dbname").unwrap();
+    let user = general.get("user").unwrap();
+    let pass = general.get("pass").unwrap();
+
+    let sslmode_ = match sslmode.as_ref() {
+        "disable" => SslMode::None,
+        "enable" => unimplemented!(),
+        _ => panic!("Wrong sslmode")
+    };
+
+    let params = ConnectParams {
+        target: ConnectTarget::Tcp(host.clone()),
+        port: FromStr::from_str(port).ok(),
+        user: Some(UserInfo {
+            user: user.clone(),
+            password: Some(pass.clone())
+        }),
+        database: Some(dbname.clone()),
+        options: vec![]
+    };
+    (params, sslmode_)
+
+}
+
 fn main() {
-    let conn = Connection::connect(
-        "postgres://postgres:postgres@localhost",
-        SslMode::None).unwrap();
-    conn.execute("CREATE TABLE person (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, data BYTEA)",&[]).unwrap();
+    let (params, sslmode) = params();
+    let conn = Connection::connect(params, sslmode).unwrap();
+    //conn.execute("CREATE TABLE person (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, data BYTEA)",&[]).unwrap();
     let me = Person {
         id: 0,
-        name: "Evgeny".to_string(),
+        name: "Jesus".to_string(),
         data: None
     };
     conn.execute("INSERT INTO person (name,data) VALUES ($1, $2)", &[&me.name, &me.data]).unwrap();
